67f912a461891197d7a0489d6e2a35107ea896b7
jamjamin <benjamjamin02@gmail.com>
2023-08-12 14:02:55 +1200

    Backend fixes

--- mern-backend/index.js
+++ mern-backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 connectDB();
 
 // cors
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: "https://cise-mern-one.vercel.app", credentials: true }));
 
 // Init Middleware
 app.use(express.json({ extended: false }));
